bd017e3e38dbe5e45dc0866293f4eec746e223bb
Aadarshprajapati777 <aadarsh9811594152@gmail.com>
2024-05-24 00:27:59 +0530

    mongo db connection config

---
+++ chat-o-mania/apps/backend/src/config.ts
@@ -0,0 +1,9 @@
+import mongoose, { ConnectOptions } from 'mongoose';
+
+export const connectToDB = () => {
+    mongoose.connect('...', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+    } as ConnectOptions);
+};
