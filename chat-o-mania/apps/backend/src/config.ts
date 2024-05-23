715f30bbe57bc26dd19a2fc51f4b051092b76e59
Aadarshprajapati777 <aadarsh9811594152@gmail.com>
2024-05-24 02:32:58 +0530

    <feat>: db connection established with mongodb atlas

--- chat-o-mania/apps/backend/src/config.ts
+++ chat-o-mania/apps/backend/src/config.ts
@@ -1,10 +1,15 @@
-import mongoose, { ConnectOptions } from 'mongoose';
+const mongoose = require('mongoose');
+import dotenv from 'dotenv';
 
-export const connectToDB = () => {
-    mongoose.connect('...', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-    } as ConnectOptions);
-};
-
+dotenv.config()
+const connectionString = process.env.DB_STRING
+const connectToDB = async () => {
+    try {
+        await mongoose.connect(connectionString, {
+            autoIndex: true
+        })
+        console.log('Connected to Mongodb Atlas');} catch (error) {
+        console.error(error);
+    }
+}
+export default connectToDB;
